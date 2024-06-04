88a86ba95800f20d7e27eb0b8f8fdcdfd8026602
Träume von Heidelberg <jsd.usis@gmail.com>
2024-06-04 19:21:56 +0200

    Fixed how electron framework works

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
-  // Tworzenie okna przeglądarki.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+function createWindow() {
+	// Tworzenie okna przeglądarki.
+	let win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
 
-  // i załaduj index.html aplikacji.
-  win.loadFile('index.html')
+	// Maksymalizacja okna po utworzeniu
+	win.maximize()
+
+	// i załaduj index.html aplikacji.
+	win.loadFile('./public/index.html')
 }
 
 app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
+})
