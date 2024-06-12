9741e2ed43e64c837079bc211e60a962fc0f2341
Träume von Heidelberg <jsd.usis@gmail.com>
2024-06-12 14:25:23 +0200

    Ja to dzis skoncze a przynajmniej w to wierze

--- associations.js
+++ associations.js
@@ -1,19 +1,19 @@
-const Reservation = require('./models/reservation');
-const Table = require('./models/table');
-const User = require('./models/user'); // Upewnij się, że ten import jest poprawny
-const News = require('./models/news');
-const NewsHeaderText = require('./models/newsHeaderText');
+const Reservation = require('./models/reservation')
+const Table = require('./models/table')
+const User = require('./models/user') // Upewnij się, że ten import jest poprawny
+const News = require('./models/news')
+const NewsHeaderText = require('./models/newsHeaderText')
 
-User.hasMany(Reservation, { foreignKey: 'userId', as: 'userReservations' });
-Reservation.belongsTo(User, { foreignKey: 'userId', as: 'reservationUser' });
+User.hasMany(Reservation, { foreignKey: 'userId', as: 'userReservations' })
+Reservation.belongsTo(User, { foreignKey: 'userId', as: 'reservationUser' })
 
-Table.hasMany(Reservation, { foreignKey: 'tableId', as: 'tableReservations' });
-Reservation.belongsTo(Table, { foreignKey: 'tableId', as: 'reservationTable' });
+Table.hasMany(Reservation, { foreignKey: 'tableId', as: 'tableReservations' })
+Reservation.belongsTo(Table, { foreignKey: 'tableId', as: 'reservationTable' })
 
 // Asocjacje dla News i User
-User.hasMany(News, { foreignKey: 'userId', as: 'createdNews' }); // Unikalny alias 'createdNews'
-News.belongsTo(User, { foreignKey: 'userId', as: 'author' }); // Unikalny alias 'author'
+User.hasMany(News, { foreignKey: 'userId', as: 'createdNews' }) // Unikalny alias 'createdNews'
+News.belongsTo(User, { foreignKey: 'userId', as: 'author' }) // Unikalny alias 'author'
 
-// Asocjacje dla News i NewsHeaderText
-News.hasMany(NewsHeaderText, { foreignKey: 'newsId', as: 'headers' }); // Unikalny alias 'headers'
-NewsHeaderText.belongsTo(News, { foreignKey: 'newsId', as: 'news' }); // Unikalny alias 'news'
+// Asocjacje dla News i NewsHeaderText z opcją CASCADE
+News.hasMany(NewsHeaderText, { foreignKey: 'newsId', as: 'headers', onDelete: 'CASCADE' })
+NewsHeaderText.belongsTo(News, { foreignKey: 'newsId', as: 'news' })
--- models/newsHeaderText.js
+++ models/newsHeaderText.js
@@ -1,16 +1,31 @@
-// models/newsHeaderText.js
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes } = require('sequelize')
+const sequelize = require('../config/database')
+const News = require('./news') // Importuj model News, aby odnieść się do klucza obcego
 
-const NewsHeaderText = sequelize.define('NewsHeaderText', {
-    header: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    text: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    }
-});
+const NewsHeaderText = sequelize.define(
+	'NewsHeaderText',
+	{
+		header: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		text: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		newsId: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: News,
+				key: 'id',
+			},
+			onDelete: 'CASCADE',
+			allowNull: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
 
-module.exports = NewsHeaderText;
+module.exports = NewsHeaderText
